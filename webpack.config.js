0757234228c92fe96c47411be0a83dc48f4fd0db
Phannt <phannguyentien1999@gmail.com>
2023-02-01 00:31:56 +0700

    xử lý caching với hash

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js'
   },
   module: {
     rules: [
@@ -19,7 +20,9 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
         title: 'webpack app',
         filename: 'index.html',
